b2ac4bfc0827e2a15f947bbd3f2c4267f7d4f795
eliezerwohl <eliezerwohl@gmail.com>
2016-01-16 16:25:34 -0500

    liri now corresponds with the omdb database

--- api/movie.js
+++ api/movie.js
@@ -1,3 +1,16 @@
-module.exports = function(){
+module.exports = function movie(){
+var request = require('request');
+var movieInput =  process.argv[3]
+console.log(process.argv[3])
+
+request("http://www.omdbapi.com/?t=a clockwork orange&y=&plot=short&r=json", function(error, response, body) {
+  // console.log(response)
+  var weatherObject = JSON.parse(body);
+  console.log(weatherObject);
+});
+
+
+
+
 console.log("omdb file");
 }
