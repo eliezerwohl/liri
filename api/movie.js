aee34e2f89685f026753ee101db2ba1ccefcfeec
eliezerwohl <eliezerwohl@gmail.com>
2016-01-17 14:28:40 -0500

    movie function now logs correct information

--- api/movie.js
+++ api/movie.js
@@ -5,10 +5,31 @@ var movieInput =  searchItem;
 
 request("http://www.omdbapi.com/?t="
   + movieInput
-  +"&y=&plot=short&r=json", function(error, response, body) {
+  +"&y=&plot=long&tomatoes=true&r=json", function(error, response, body) {
+
   // console.log(response)
   var movieObject = JSON.parse(body);
-  console.log(movieObject);
+  // console.log(movieObject);
+    console.log(movieObject.Title)
+    console.log(movieObject.Year)
+    console.log(movieObject.imdbRating)
+    console.log(movieObject.Country)
+    console.log(movieObject.Language)
+    console.log(movieObject.Plot)
+    console.log(movieObject.Actors)
+    console.log(movieObject.tomatoUserRating)
+    console.log(movieObject.tomatoURL)
+
+//   Title
+// Year
+// IMDB Rating
+// Country
+// Language
+// Plot
+// Actors
+// Rotten Tomatoes Rating
+// Rotton Tomatoes UrL
+
 });
 
 
